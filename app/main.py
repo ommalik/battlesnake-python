128fc2f42bf68ecc5ba83d7593a0226dc22e968c
ommalik <omalik@uvic.ca>
2016-02-20 12:39:06 -0800

    initalization before we start

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ def index():
     )
 
     return {
-        'color': '#00ff00',
+        'color': '#112233',
         'head': head_url
     }
 
@@ -27,7 +27,7 @@ def start():
     # TODO: Do things with data
 
     return {
-        'taunt': 'battlesnake-python!'
+        'taunt': 'Super Snake Action Team!'
     }
 
 
@@ -39,7 +39,7 @@ def move():
 
     return {
         'move': 'north',
-        'taunt': 'battlesnake-python!'
+        'taunt': 'SSAT Moves north'
     }
 
 
